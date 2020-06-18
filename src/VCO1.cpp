b9f51fbb23e06747387341d9c51919fc86aea97a
bgf777 <bruce@mowrite.net>
2020-06-17 22:52:19 -0700

    -

--- src/VCO1.cpp
+++ src/VCO1.cpp
@@ -7,31 +7,29 @@
 struct VCO1Module : Module
 {
     enum ParamIds {
-		NUM_PARAMS
+        NUM_PARAMS
 	};
 	enum InputIds {
-		NUM_INPUTS
+        NUM_INPUTS
 	};
 	enum OutputIds {
-		NUM_OUTPUTS
+        NUM_OUTPUTS
 	};
 	enum LightIds {
-		NUM_LIGHTS
+        NUM_LIGHTS
     };
 
     VCO1Module() {
-		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+        config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		//for (int i = 0; i < 8; i++) {
 		//	configParam(GAIN_PARAMS + i, -1.f, 1.f, 0.f, string::f("Ch %d gain", i + 1), "%", 0, 100);
 		//}
-	}
+    }
 };
 
 struct VCO1Widget : ModuleWidget {
     VCO1Widget(VCO1Module* module) {
-        
     }
 };
-
 
 Model* modelVCO1 = createModel<VCO1Module, VCO1Widget>("demo-vco1");
