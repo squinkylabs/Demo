762cd435780ffa31e0307cf807b2dc32d645f4d1
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2021-12-11 14:08:10 -0800

    -

--- src/VCOB1.cpp
+++ src/VCOB1.cpp
@@ -5,41 +5,38 @@
  */
 
 #include "demo-plugin.hpp"
- 
-// VCV has a limit of 16 channels in a polyphonic cable.
-static const int maxPolyphony = engine::PORT_MAX_CHANNELS;
+
+#define OVERSAMPLING_FACTOR 8
 
 /**
  *  Every synth module must have a Module structure.
  *  This is where all the real-time processing code goes.
  */
-struct VCOB1Module : Module
-{
+struct VCOB1Module : Module {
     enum ParamIds {
         PITCH_PARAM,
         NUM_PARAMS
-	};
-	enum InputIds {
+    };
+    enum InputIds {
         CV_INPUT,
         NUM_INPUTS
-	};
-	enum OutputIds {
-        SAW_OUTPUT,
-        SIN_OUTPUT,
-        PARA_OUTPUT,
+    };
+    enum OutputIds {
+        MAIN_OUTPUT,
+        TEST_OUTPUT,
         NUM_OUTPUTS
-	};
-	enum LightIds {
+    };
+    enum LightIds {
         NUM_LIGHTS
     };
 
-    float phaseAccumulators[maxPolyphony] = {};
-    float phaseAdvance[maxPolyphony] = {};
-    int currentPolyphony = 1;
-    int loopCounter = 0;
-    bool outputSaw = false;
-    bool outputSin = false;
-    bool outputPara = false;
+    float phaseAccumulatorTest = 0;
+    float phaseAdvanceTest = 0;
+    float phaseAccumulatorMain = 0;
+    float phaseAdvanceMain = 0;
+
+    // int loopCounter = 0;
+    int oversampleCounter = 1;
 
     VCOB1Module() {
         // Your module must call config from its constructor, passing in
@@ -51,7 +48,7 @@ struct VCOB1Module : Module
     // Every Module has a process function. This is called once every
     // sample, and must service all the inputs and outputs of the module.
     void process(const ProcessArgs& args) override {
-
+#if 0
         // There are usually some thing that don't need to be done every single sample.
         // For example: looking at a knob position. You can some CPU if you do 
         // this less often.
@@ -65,85 +62,70 @@ struct VCOB1Module : Module
         }
 
         generateOutput();
+#endif
+        updateParams(args);
+        doOutput();
     }
 
-    void processEvery4Samples(const ProcessArgs& args) {
-        // Is is very important that you tell the output ports how
-        // many channels the should export. In a VCO it is very common
-        // to use the number of CV input channels to automatically determine the
-        // polyphony.
-        // It is also very common to always run one channel, even if there is
-        // no input. This lets the VCO generate output with no input.
-        currentPolyphony = std::max(1, inputs[CV_INPUT].getChannels());
-        outputs[SIN_OUTPUT].setChannels(currentPolyphony);
-        outputs[SAW_OUTPUT].setChannels(currentPolyphony);
-        outputs[PARA_OUTPUT].setChannels(currentPolyphony);
+    void updateParams(const ProcessArgs& args) {
+        phaseAdvanceTest = computePhaseAdvance(args.sampleTime);
+        phaseAdvanceMain = computePhaseAdvance(args.sampleTime);
+    }
 
-        //now we are going to look at our input and parameters and
-        // save off some values for out audio processing function.
-        outputSaw = outputs[SAW_OUTPUT].isConnected();
-        outputSin = outputs[SIN_OUTPUT].isConnected();
-        outputPara = outputs[PARA_OUTPUT].isConnected();
+    void doOutput() {
+        generateBasic();
+        generatePartialOversample1();
+    }
+
+    void generateBasic() {
+        advancePhase( phaseAccumulatorTest, phaseAdvanceTest);
+        float sawWave = phaseToSaw(phaseAccumulatorTest);
+        outputs[TEST_OUTPUT].setVoltage(sawWave, 0);
+    }
+
+    void generatePartialOversample1() {
+        float output = 0;
+        if (oversampleCounter-- == 0) {
+            oversampleCounter = (OVERSAMPLING_FACTOR - 1);
+
+            advancePhase(phaseAccumulatorMain, phaseAdvanceMain);
+            output = phaseToSaw(phaseAccumulatorMain);  // * OVERSAMPLING_FACTOR;
+        }
+        // float sawWave = phaseToSaw(phaseAccumulatorMain);
+        outputs[MAIN_OUTPUT].setVoltage(output, 0);
+    }
+
+    static void advancePhase(float& accumulator, const float& phaseAdvance) {
+        accumulator += phaseAdvance;
+        if (accumulator > 1.f) {
+            // We limit our phase to the range 0..1
+            accumulator -= 1.f;
+        }
+    }
+
+    static float phaseToSaw(float phase) {
+        // return (phase - .5f) * 10;
+        float x = phase > .5 ? (1 - phase) : phase;
+        return (x - .25) * 10;
+    }
+
+    float computePhaseAdvance(float sampleTime) {
+        float pitchCV = inputs[CV_INPUT].getVoltage(0);
         float pitchParam = params[PITCH_PARAM].value;
-        for (int i = 0; i < currentPolyphony; ++i) {
-            float pitchCV = inputs[CV_INPUT].getVoltage(i);
-            float combinedPitch = pitchParam + pitchCV - 4.f;
+        float combinedPitch = pitchParam + pitchCV - 4.f;
 
-            const float q = float(std::log2(261.626));       // move up to C
-            combinedPitch += q;
+        const float q = float(std::log2(261.626));  // move up to C
+        combinedPitch += q;
 
-            // combined pitch is in volts. Now use the pow function
-            // to convert that to a pitch. Not: there are more efficient ways
-            // to do this than use std::pow.
-            const float freq = std::pow(2.f, combinedPitch);
+        // combined pitch is in volts. Now use the pow function
+        // to convert that to a pitch. Not: there are more efficient ways
+        // to do this than use std::pow.
+        const float freq = std::pow(2.f, combinedPitch);
 
-            // figure out how much to add to our ramp every cycle 
-            // to make a saw at the desired frequency.
-            const float normalizedFreq = args.sampleTime * freq;
-            phaseAdvance[i] = normalizedFreq;
-        }
-    }
-
-    void generateOutput() {
-        for (int i = 0; i < currentPolyphony; ++i) {
-            // Every sample, we advance the phase of our ramp by the amount
-            // we derived from the CV and knob inputs.
-            phaseAccumulators[i] += phaseAdvance[i];
-            if (phaseAccumulators[i] > 1.f) {
-                // We limit our phase to the range 0..1
-                phaseAccumulators[i] -= 1.f;
-            }
-
-            if (outputSaw) {
-                // If the saw output it patched, turn our 0..1 ramp
-                // into a -5..+5 sawtooth. This  math is very easy!
-                float sawWave = (phaseAccumulators[i] - .5f) * 10;
-                outputs[SAW_OUTPUT].setVoltage(sawWave, i);
-            }
-
-            if (outputSin) {
-                // If the sin output it patched, turn our 0..1 ramp
-                // into a -5..+5 sine. This  math is less easy!
-
-                // First convert 0..1 0..2pi (convert to radian angles)
-                float radianPhase = phaseAccumulators[i] * 2 * float(M_PI);
-
-                // sin of 0..2pi will be a sinewave from -1 to 1.
-                // Easy to convert to -5 to +5
-                float sinWave = std::sin(radianPhase) * 5;
-                outputs[SIN_OUTPUT].setVoltage(sinWave, i);
-            }
-
-            if (outputPara) {
-                // This simple "parabolic ramp" is an example of a way one could try to 
-                // make the sawtooth sound a little different.
-                float paraWave = phaseAccumulators[i];
-                paraWave *= paraWave;
-                paraWave -= .33f;       // subtract out the DC component (use your calculus or trial and error).
-                paraWave *= 10;
-                outputs[PARA_OUTPUT].setVoltage(paraWave, i);
-            }
-        }
+        // figure out how much to add to our ramp every cycle
+        // to make a saw at the desired frequency.
+        const float normalizedFreq = sampleTime * freq;
+        return normalizedFreq;
     }
 };
 
@@ -159,19 +141,19 @@ struct VCOB1Widget : ModuleWidget {
         // you must call this function first in your widget constructor.
         setModule(module);
 
-        // Typically the panel graphic is added first, then the other 
+        // Typically the panel graphic is added first, then the other
         // UI elements are placed on TOP.
         // In VCV the Z-order of added children is such that later
         // children are always higher than children added earlier.
-		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/vco1_panel.svg")));
+        setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/vco1_panel.svg")));
 
         // VCV modules usually have image is "screws" to make them
-        // look more like physical module. You may design your own screws, 
+        // look more like physical module. You may design your own screws,
         // or not use screws at all.
-		addChild(createWidget<ScrewSilver>(Vec(15, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 30, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(15, 365)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 30, 365)));
+        addChild(createWidget<ScrewSilver>(Vec(15, 0)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 30, 0)));
+        addChild(createWidget<ScrewSilver>(Vec(15, 365)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 30, 365)));
 
         // It's purely personal style whether you want to set variables like this
         // For the position of your widgets. It's fine to do it inline also.
@@ -181,7 +163,7 @@ struct VCOB1Widget : ModuleWidget {
         float knobY = 130;
         float sawY = 190;
         float sinY = 240;
-        float paraY = 290;
+        //float paraY = 290;
         float labelAbove = 20;
 
         // Now we place the widgets that represent the inputs, outputs, controls,
@@ -189,18 +171,16 @@ struct VCOB1Widget : ModuleWidget {
         // the other widgets.
 
         addInput(createInput<PJ301MPort>(Vec(x, inputY), module, VCOB1Module::CV_INPUT));
-        addParam(createParam<RoundBlackKnob>(Vec(x-4, knobY), module, VCOB1Module::PITCH_PARAM));
-        addOutput(createOutput<PJ301MPort>(Vec(x, sawY), module, VCOB1Module::SAW_OUTPUT));
-        addOutput(createOutput<PJ301MPort>(Vec(x, sinY), module, VCOB1Module::SIN_OUTPUT));
-        addOutput(createOutput<PJ301MPort>(Vec(x, paraY), module, VCOB1Module::PARA_OUTPUT));
-    
+        addParam(createParam<RoundBlackKnob>(Vec(x - 4, knobY), module, VCOB1Module::PITCH_PARAM));
+        addOutput(createOutput<PJ301MPort>(Vec(x, sawY), module, VCOB1Module::MAIN_OUTPUT));
+        addOutput(createOutput<PJ301MPort>(Vec(x, sinY), module, VCOB1Module::TEST_OUTPUT));
+
         // Add some quick hack labels to the panel.
-        addLabel(Vec(20, headingY), "Demo VCO1");
-        addLabel(Vec(x-16, inputY - labelAbove), "Pitch CV");
-        addLabel(Vec(x-10, knobY - labelAbove), "Pitch");
-        addLabel(Vec(x-16, sawY - labelAbove), "Saw Out");
-        addLabel(Vec(x-16, sinY - labelAbove), "Sin Out");
-        addLabel(Vec(x-16, paraY - labelAbove), "Para Out");
+        addLabel(Vec(20, headingY), "Demo VCO B");
+        addLabel(Vec(x - 16, inputY - labelAbove), "Pitch CV");
+        addLabel(Vec(x - 10, knobY - labelAbove), "Pitch");
+        addLabel(Vec(x - 16, sawY - labelAbove), "Main Out");
+        addLabel(Vec(x - 16, sinY - labelAbove), "Test Out");
     }
 
     // Simple helper function to add test labels to the panel.
@@ -208,9 +188,8 @@ struct VCOB1Widget : ModuleWidget {
     // Labels are fine for hacking, but they are discouraged for real use.
     // Some of the problems are that they don't draw particularly efficiently,
     // and they don't give as much control as putting them into the panel SVG.
-    Label* addLabel(const Vec& v, const std::string& str)
-    {
-        NVGcolor black = nvgRGB(0,0,0);
+    Label* addLabel(const Vec& v, const std::string& str) {
+        NVGcolor black = nvgRGB(0, 0, 0);
         Label* label = new Label();
         label->box.pos = v;
         label->text = str;
@@ -221,7 +200,7 @@ struct VCOB1Widget : ModuleWidget {
 };
 
 // This mysterious line must appear for each module. The
-// name in quotes at then end is the same string that will be in 
+// name in quotes at then end is the same string that will be in
 // plugin.json in the entry for corresponding plugin.
 
 // This line basically tells VCV Rack:
