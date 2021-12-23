bbd8570b9cf37d2abb78959b9d0c7fabec4daaff
U-DESKTOP-Q45DVE8\bruce <bruce@mowrite.net>
2021-12-23 11:37:33 -0800

    -

--- dsp/Filter6pButter.h
+++ dsp/Filter6pButter.h
@@ -6,8 +6,8 @@
 class Filter6PButter {
 public:
     void setCutoffFreq(float x) {
-        INFO("set cutoff freq %f", x);
         assert(x > 0 && x < .5f);
+        // calculator https://www.earlevel.com/main/2016/09/29/cascading-filters/
         f[0].setParameters(rack::dsp::TBiquadFilter<float>::LOWPASS, x, .51763809, 1);
         f[1].setParameters(rack::dsp::TBiquadFilter<float>::LOWPASS, x, 0.70710678, 1);
         f[2].setParameters(rack::dsp::TBiquadFilter<float>::LOWPASS, x, 1.9318517, 1);
@@ -27,9 +27,8 @@ private:
 class Filter12PButter {
 public:
     void setCutoffFreq(float x) {
-        INFO("set cutoff freq %f", x);
         assert(x > 0 && x < .5f);
-        // calulator https://www.earlevel.com/main/2016/09/29/cascading-filters/
+        // calculator https://www.earlevel.com/main/2016/09/29/cascading-filters/
         f[0].setParameters(rack::dsp::TBiquadFilter<float>::LOWPASS, x, .50411961, 1);
         f[1].setParameters(rack::dsp::TBiquadFilter<float>::LOWPASS, x, 0.541196108, 1);
         f[2].setParameters(rack::dsp::TBiquadFilter<float>::LOWPASS, x, 0.63023621, 1);
